0bde90f79363a205be2d783ccc5fa11d9ddb5bc9
usrname <usrnamewastaken000@gmail.com>
2021-02-20 20:08:39 -0600

    added autohide + keypress patches

--- config.h
+++ config.h
@@ -33,16 +33,20 @@ static Bool npisrelative  = False;
         } \
 }
 
+/* Keybinding anything to CONTROL+SHIFT should be avoided. */
+/* CONTROL+SHIFT is used extensively in the surf browser. */
+/* Use CONTROL+ALT instead! */
+
 #define MODKEY ControlMask
 static Key keys[] = {
 	/* modifier             key        function     argument */
-	{ MODKEY|ShiftMask,     XK_Return, focusonce,   { 0 } },
-	{ MODKEY|ShiftMask,     XK_Return, spawn,       { 0 } },
+	{ MODKEY|Mod1Mask,      XK_Return, focusonce,   { 0 } },
+	{ MODKEY|Mod1Mask,      XK_Return, spawn,       { 0 } },
 
-	{ MODKEY|ShiftMask,     XK_l,      rotate,      { .i = +1 } },
-	{ MODKEY|ShiftMask,     XK_h,      rotate,      { .i = -1 } },
-	{ MODKEY|ShiftMask,     XK_j,      movetab,     { .i = -1 } },
-	{ MODKEY|ShiftMask,     XK_k,      movetab,     { .i = +1 } },
+	{ MODKEY|Mod1Mask,      XK_l,      rotate,      { .i = +1 } },
+	{ MODKEY|Mod1Mask,      XK_h,      rotate,      { .i = -1 } },
+	{ MODKEY|Mod1Mask,      XK_j,      movetab,     { .i = -1 } },
+	{ MODKEY|Mod1Mask,      XK_k,      movetab,     { .i = +1 } },
 	{ MODKEY,               XK_Tab,    rotate,      { .i = 0 } },
 
 	{ MODKEY,               XK_grave,  spawn,       SETPROP("_TABBED_SELECT_TAB") },
@@ -57,10 +61,21 @@ static Key keys[] = {
 	{ MODKEY,               XK_9,      move,        { .i = 8 } },
 	{ MODKEY,               XK_0,      move,        { .i = 9 } },
 
-	{ MODKEY,               XK_q,      killclient,  { 0 } },
+	{ MODKEY|Mod1Mask,      XK_q,      killclient,  { 0 } },
 
 	{ MODKEY,               XK_u,      focusurgent, { 0 } },
-	{ MODKEY|ShiftMask,     XK_u,      toggle,      { .v = (void*) &urgentswitch } },
+	{ MODKEY|Mod1Mask,      XK_u,      toggle,      { .v = (void*) &urgentswitch } },
 
 	{ 0,                    XK_F11,    fullscreen,  { 0 } },
+
+	{ MODKEY,               XK_Alt_L,     showbar,    { .i = 1 } },
+	{ Mod1Mask,             XK_Control_L, showbar,  { .i = 1 } },
+	{ MODKEY,               XK_period,    showbar,     { .i = 1 } },
+	{ MODKEY,               XK_comma,     showbar,      { .i = 0 } },
+};
+
+static Key keyreleases[] = {
+	/* modifier             key           function  argument */
+	{ MODKEY|Mod1Mask,      XK_Alt_L,     showbar,  { .i = 0 } },
+	{ MODKEY|Mod1Mask,      XK_Control_L, showbar,  { .i = 0 } },
 };
